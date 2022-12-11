3bb04a08c8b8c155ba75b689938de3b963644b29
Philip Jambrisak <jambrisak@gmail.com>
2022-12-11 19:30:49 +0100

    chore: setup a function for the boards

--- run.py
+++ run.py
@@ -3,3 +3,7 @@
 #Guess board will hold hits or misses
 HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
+
+#Function to print the boards
+def board():
+    pass
