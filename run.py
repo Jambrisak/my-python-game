942961bbea3a3f3baf9f7714fb6750df1f34f714
Philip Jambrisak <jambrisak@gmail.com>
2022-12-11 19:42:38 +0100

    Update: Fixed the function to print the board

--- run.py
+++ run.py
@@ -8,8 +8,13 @@ GUESS_BOARD = [[' '] * 8 for x in range(8)]
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 #Function to print the boards
-def board():
-    pass
+def print_board(board):
+    print('     A B C D E F G H')
+    print('     ---------------')
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
 
 #Function to create ships
 def create_ships():
